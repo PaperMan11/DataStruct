9f6256c11f29251c48c2bf1f9ede615210a59456
ziqing <94894033+PaperMan11@users.noreply.github.com>
2022-12-13 14:16:56 +0800

    Create kvnode.go

---
+++ datastruct/bptree/kvnode.go
@@ -0,0 +1,15 @@
+package bptree
+
+// KvNode BPNode中的成员
+type KvNode struct {
+	key   int64
+	value interface{}
+}
+
+// newKvNode 新建KvNode成员
+func newKvNode(key int64, value interface{}) *KvNode {
+	return &KvNode{
+		key:   key,
+		value: value,
+	}
+}
