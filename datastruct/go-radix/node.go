ed672a357a7988f70c54d60facba95eb8a37f4b7
ziqing <94894033+PaperMan11@users.noreply.github.com>
2023-01-12 17:02:56 +0800

    Create node.go

---
+++ datastruct/go-radix/node.go
@@ -0,0 +1,98 @@
+package goradix
+
+import "sort"
+
+type leafNode struct {
+	key string // 完整值
+	val interface{}
+}
+
+// edges 边缘节点
+type edge struct {
+	label byte // 存储首字母
+	node  *node
+}
+
+type edges []edge
+
+func (e edges) Len() int {
+	return len(e)
+}
+
+func (e edges) Less(i, j int) bool {
+	return e[i].label < e[j].label
+}
+
+func (e edges) Swap(i, j int) {
+	e[i], e[j] = e[j], e[i]
+}
+
+func (e edges) Sort() {
+	sort.Sort(e)
+}
+
+type node struct {
+	leaf   *leafNode
+	prefix string
+	edges  edges // 边缘应该按顺序存储以便迭代
+}
+
+func (n *node) isLeaf() bool {
+	return n.leaf != nil
+}
+
+func (n *node) addEdge(e edge) {
+	num := len(n.edges)
+	// 找到插入点
+	idx := sort.Search(num, func(i int) bool {
+		return n.edges[i].label >= e.label
+	})
+	n.edges = append(n.edges, edge{})
+	copy(n.edges[idx+1:], n.edges[idx:])
+	n.edges[idx] = e
+}
+
+func (n *node) updateEdge(label byte, node *node) {
+	num := len(n.edges)
+	idx := sort.Search(num, func(i int) bool {
+		return n.edges[i].label >= label
+	})
+	if idx < num && n.edges[idx].label == label {
+		n.edges[idx].node = node
+		return
+	}
+	panic("replacing missing edge")
+}
+
+func (n *node) getEdge(label byte) *node {
+	num := len(n.edges)
+	idx := sort.Search(num, func(i int) bool {
+		return n.edges[i].label >= label
+	})
+	if idx < num && n.edges[idx].label == label {
+		return n.edges[idx].node
+	}
+	return nil
+}
+
+func (n *node) delEdge(label byte) {
+	num := len(n.edges)
+	idx := sort.Search(num, func(i int) bool {
+		return n.edges[i].label >= label
+	})
+	if idx < num && n.edges[idx].label == label {
+		copy(n.edges[idx:], n.edges[idx+1:])
+		n.edges[len(n.edges)-1] = edge{}
+		n.edges = n.edges[:len(n.edges)-1]
+	}
+}
+
+// mergeChild 与第一个孩子合并
+// 用于压缩 tree
+func (n *node) mergeChild() {
+	e := n.edges[0]
+	child := e.node
+	n.prefix += child.prefix
+	n.leaf = child.leaf
+	n.edges = child.edges
+}
