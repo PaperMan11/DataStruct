88044317e388e3b64c2ea307e18672d2395e7479
ziqing <94894033+PaperMan11@users.noreply.github.com>
2023-02-09 15:42:02 +0800

    add QSort3Ways

--- algorithm/sort/quicksort.go
+++ algorithm/sort/quicksort.go
@@ -29,3 +29,43 @@ func quickSort(arr []int64, start, end int) {
 		}
 	}
 }
+
+// 三路快速排序
+func QuickSort3Ways(arr []int64) {
+	quickSort3Ways(arr, 0, len(arr)-1)
+}
+
+func quickSort3Ways(arr []int64, l, r int) {
+	if l >= r {
+		return
+	}
+
+	var (
+		lt    = l     // arr[l+1...lt] < pivot
+		gt    = r + 1 // arr[gt...r] > pivot
+		i     = l     // arr[lt+1...i) == pivot
+		pivot = arr[l]
+	)
+	for i < gt {
+		if arr[i] < pivot {
+			swap(arr, l, i)
+			i++
+			lt++
+		} else if arr[i] > pivot {
+			swap(arr, i, gt-1)
+			gt--
+		} else {
+			i++
+		}
+	}
+	if arr[l] > arr[lt] {
+		swap(arr, l, lt)
+	}
+	quickSort3Ways(arr, l, lt)
+	quickSort3Ways(arr, gt, r)
+}
+
+func swap(arr []int64, i, j int) {
+	arr[i], arr[j] = arr[j], arr[i]
+}
+
